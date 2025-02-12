3880f160bacab1bd4990926e89d6409ba739108c
rex <1073853456@qq.com>
2025-02-13 00:38:41 +0800

    add repeat apply

--- MyTactics/Basic.lean
+++ MyTactics/Basic.lean
@@ -1,1 +1,5 @@
-def hello := "world"
+macro "repeat_apply" t₁:term "or" t₂:term : tactic =>
+`(tactic | repeat (first |  apply $t₁ | apply $t₂) )
+
+example : 2 ≤ 10 := by
+  repeat_apply Nat.le_refl or Nat.le_step
